92ac65234f0c83081587404c9fc0f8771399cb83
CheorHyeon <126079049+CheorHyeon@users.noreply.github.com>
2023-03-06 19:43:39 +0900

    1단ê³단계 수정 (#4)
    Good!

--- src/test/java/org/example/TestUtil.java
+++ src/test/java/org/example/TestUtil.java
@@ -1,12 +1,35 @@
 package org.example;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
 import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestUtil {
     public static Scanner genScanner(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         return new Scanner(in);
+    }
+
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try{
+            output.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
