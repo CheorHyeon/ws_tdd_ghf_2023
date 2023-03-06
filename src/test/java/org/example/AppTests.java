1cef73cddc8b582fea7e6bc70ea571154bf5ba3e
깃ChertHyeon <r4560798@naver.com>
2023-03-06 22:54:51 +0900

    6단계 삭제 기능 구현

--- src/test/java/org/example/AppTests.java
+++ src/test/java/org/example/AppTests.java
@@ -113,8 +113,8 @@ public class AppTests {
     }
 
     @Test
-    @DisplayName("목록 출력")
-    public void t7() {
+    @DisplayName("1번 명언 삭제")
+    public void t8() {
         String rs = AppTestRunner.run("""
                 등록
                 현재를 사랑하라.
@@ -123,13 +123,37 @@ public class AppTests {
                 과거에 집착하지 마라.
                 작자미상
                 목록
+                삭제?id=1
                 """.stripIndent().trim());
 
         assertThat(rs)
                 .contains("번호 / 작가 / 명언")
                 .contains("______________________________")
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
-                .contains("1 / 작자미상 / 현재를 사랑하라.");
+                .contains("1 / 작자미상 / 현재를 사랑하라.")
+                .contains("1번 명언이 삭제되었습니다.");
+    }
+
+    @Test
+    @DisplayName("1번 명언 삭제")
+    public void t8() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                삭제?id=1
+                """.stripIndent().trim());
+
+        assertThat(rs)
+                .contains("번호 / 작가 / 명언")
+                .contains("______________________________")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.")
+                .contains("1번 명언이 삭제되었습니다.");
     }
 
 }
