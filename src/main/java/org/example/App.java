411c4abd065959ef505a08e5799f6682d42d2970
깃ChertHyeon <r4560798@naver.com>
2023-03-06 22:03:15 +0900

    명언 등록시 마다 번호 증가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class App {
@@ -10,15 +11,27 @@ public class App {
 
     public void run() {
         System.out.println("== 명언앱 ==");
-        System.out.println("명령) ");
+
+        long lastWiseSayingId = 0;
 
         while  (true) {
+            System.out.println("명령) ");
             String cmd = sc.nextLine().trim();
 
             switch (cmd) {
                 case "종료":
                     System.out.println("프로그램이 종료되었습니다.");
                     return;
+                case "등록" :
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.println("작가 : ");
+                    String authorName = sc.nextLine().trim();
+
+                    long id = ++lastWiseSayingId;
+                    System.out.printf("%d번 명언이 등록되었습니다.", id);
+                    break;
+
                 default:
                     System.out.println("올바르지 않은 명령입니다.");
                     break;
