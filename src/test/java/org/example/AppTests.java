2ffb31d08c4e80349a9c7ff6d93eff7111709cdb
깃ChertHyeon <r4560798@naver.com>
2023-03-06 22:57:36 +0900

    7단계, 존재하지 않는 명언삭제에 대한 예외처리

--- src/test/java/org/example/AppTests.java
+++ src/test/java/org/example/AppTests.java
@@ -136,7 +136,7 @@ public class AppTests {
 
     @Test
     @DisplayName("1번 명언 삭제")
-    public void t8() {
+    public void t9() {
         String rs = AppTestRunner.run("""
                 등록
                 현재를 사랑하라.
@@ -154,6 +154,30 @@ public class AppTests {
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
                 .contains("1 / 작자미상 / 현재를 사랑하라.")
                 .contains("1번 명언이 삭제되었습니다.");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 명언삭제에 대한 예외처리")
+    public void t10() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                삭제?id=1
+                삭제?id=1
+                """.stripIndent().trim());
+
+        assertThat(rs)
+                .contains("번호 / 작가 / 명언")
+                .contains("______________________________")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.")
+                .contains("1번 명언이 삭제되었습니다.")
+                .contains("1번 명언은 존재하지 않습니다.");
     }
 
 }
